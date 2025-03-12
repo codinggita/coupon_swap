5462f3c6a1618471ba390677dc8ce086c290f5b5
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-12 10:46:31 +0530

    Small changes

--- Frontend/src/assets/components/Body.jsx
+++ Frontend/src/assets/components/Body.jsx
@@ -323,8 +323,6 @@ const Body = () => {
           </div>
         </div>
       </section>
-
-
       {/* Payment Modal */}
       {showPaymentModal && selectedCoupon && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
