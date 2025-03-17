795b0fe9319417e5b8ccb942264a4156874972c1
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-17 11:04:12 +0530

    add the signin and login routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const validateUser = require('../middleware/validation');
+
+router.post('/signin', validateUser, authController.signUp);
+module.exports = router;
