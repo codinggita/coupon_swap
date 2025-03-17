9ed4871657d6440c37ce5629ec69a05f87722474
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-17 10:53:51 +0530

    Add user login in schema

--- backend/models/login.js
+++ backend/models/login.js
@@ -14,6 +14,5 @@ userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
-
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 module.exports = User;
