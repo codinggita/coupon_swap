b141c6f8179acda6a32ffbd48fcd5580ef30d359
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-17 11:20:03 +0530

    add the login routes add

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const { login } = require('../controllers/loginController');
-
 const router = express.Router();
-
 router.post('/login', login);
-
 module.exports = router;
