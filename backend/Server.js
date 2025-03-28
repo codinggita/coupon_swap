8bdace97a8666c9b66fadd89c06eda3e4883baa0
Nageshjagtap <nageshjagtap063@gmail.com>
2025-03-28 23:09:23 +0530

    offer details section add

--- backend/Server.js
+++ backend/Server.js
@@ -1,15 +1,18 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));  
+app.use(cors({ origin: 'http://localhost:5175', credentials: true }));
 app.use(express.json());
 
 connectDB();
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/auth', require('./routes/loginRoutes'));
+
+
+app.use('/api/auth', require('./routes/authRoutes'));    
+app.use('/api/auth', require('./routes/loginRoutes'));   
+app.use('/api/coupon', require('./routes/couponRoutes')); 
+app.use('/api/profile', require('./routes/profileRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
